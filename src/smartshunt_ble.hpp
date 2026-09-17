ffbfe11e51ffed399cfb757ce3bd8569ac629fc7
cram001 <marcarchambault@gmail.com>
2026-09-17 09:44:16 -0700

    Track SmartShunt field validity and stale data

--- src/smartshunt_ble.hpp
+++ src/smartshunt_ble.hpp
@@ -11,13 +11,20 @@ struct SmartShuntData {
     int rssi = 0;
     uint32_t age_ms = 0;
     uint16_t alarm_reason = 0;
+
+    bool time_to_go_valid = false;
     uint16_t time_to_go_min = 0;
+    bool voltage_valid = false;
     float voltage_v = 0.0f;
+    bool current_valid = false;
     float current_a = 0.0f;
+    bool consumed_ah_valid = false;
     float consumed_ah = 0.0f;
+    bool soc_valid = false;
     float soc_pct = 0.0f;
     bool temperature_valid = false;
     float temperature_c = 0.0f;
+
     char source_mac[18]{};
 };
 
