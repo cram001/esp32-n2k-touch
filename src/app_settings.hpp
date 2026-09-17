c8aea187b51661fd0132388904d4837bdb20d995
cram001 <marcarchambault@gmail.com>
2026-09-17 10:07:42 -0700

    Refactor settings for multiple SmartShunts

--- src/app_settings.hpp
+++ src/app_settings.hpp
@@ -3,21 +3,28 @@
 #include <array>
 #include <cstdint>
 
+constexpr size_t MAX_SMARTSHUNTS = 4;
+
 enum class DisplayTheme : uint8_t {
     Day = 0,
     Night = 1,
+};
+
+struct SmartShuntConfig {
+    bool configured = false;
+    bool enabled = true;
+    bool n2k_enabled = false;
+    uint8_t battery_instance = 0;
+    std::array<char, 25> name{};
+    std::array<char, 18> mac{};       // Internal stable identity; normally hidden from UI.
+    std::array<char, 33> bindkey{};   // 128-bit key as 32 hex chars.
 };
 
 struct AppSettings {
     DisplayTheme theme = DisplayTheme::Day;
     uint8_t day_brightness = 80;
     uint8_t night_brightness = 20;
-
-    bool smartshunt_enabled = false;
-    bool smartshunt_n2k_enabled = false;
-    uint8_t battery_instance = 0;
-    std::array<char, 18> smartshunt_mac{};       // "AA:BB:CC:DD:EE:FF"
-    std::array<char, 33> smartshunt_bindkey{};   // 128-bit key as 32 hex chars
+    std::array<SmartShuntConfig, MAX_SMARTSHUNTS> smartshunts{};
 };
 
 bool settings_init();
