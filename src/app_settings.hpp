10396d8c03bf03ac79dbb24c5461e4ff2a0fe5f7
cram001 <marcarchambault@gmail.com>
2026-09-17 11:56:47 -0700

    Add configurable instrument pages and units model

--- src/app_settings.hpp
+++ src/app_settings.hpp
@@ -5,10 +5,78 @@
 #include <cstdint>
 
 constexpr size_t MAX_SMARTSHUNTS = 4;
+constexpr size_t MAX_DATA_PAGES = 6;
+constexpr size_t MAX_DATA_FIELDS_PER_PAGE = 6;
 
 enum class DisplayTheme : uint8_t {
     Day = 0,
     Night = 1,
+};
+
+enum class PageLayout : uint8_t {
+    One = 1,
+    Two = 2,
+    Four = 4,
+    Six = 6,
+};
+
+enum class DataSourceType : uint8_t {
+    Nmea2000 = 0,
+    SmartShunt = 1,
+};
+
+enum class DataMetric : uint8_t {
+    None = 0,
+    Depth,
+    BoatSpeed,
+    SpeedOverGround,
+    CourseOverGround,
+    Heading,
+    ApparentWindSpeed,
+    ApparentWindAngle,
+    TrueWindSpeed,
+    TrueWindAngle,
+    WaterTemperature,
+    AirTemperature,
+    DistanceToWaypoint,
+    TripDistance,
+    BatteryVoltage,
+    BatteryCurrent,
+    BatterySoc,
+    BatteryConsumedAh,
+    BatteryTimeToGo,
+    BatteryTemperature,
+};
+
+enum class DepthUnit : uint8_t { Metres = 0, Feet = 1 };
+enum class TemperatureUnit : uint8_t { Celsius = 0, Fahrenheit = 1 };
+enum class SpeedUnit : uint8_t { Knots = 0, KilometresPerHour = 1, MetresPerSecond = 2 };
+enum class DistanceUnit : uint8_t { NauticalMiles = 0, Kilometres = 1 };
+enum class ShortDistanceUnit : uint8_t { Metres = 0, Feet = 1, Yards = 2 };
+
+struct UnitsSettings {
+    DepthUnit depth = DepthUnit::Metres;
+    TemperatureUnit temperature = TemperatureUnit::Celsius;
+    SpeedUnit wind_speed = SpeedUnit::Knots;
+    SpeedUnit vessel_speed = SpeedUnit::Knots;
+    DistanceUnit distance = DistanceUnit::NauticalMiles;
+    ShortDistanceUnit short_distance = ShortDistanceUnit::Metres;
+    // Distances below this threshold use short_distance. Stored in nautical miles.
+    float short_distance_threshold_nm = 0.2f;
+};
+
+struct DataFieldSelection {
+    DataSourceType source = DataSourceType::Nmea2000;
+    DataMetric metric = DataMetric::None;
+    // Used for SmartShunt source; ignored for NMEA 2000 fields.
+    uint8_t source_index = 0;
+};
+
+struct DataPageConfig {
+    bool enabled = false;
+    PageLayout layout = PageLayout::Four;
+    std::array<char, 17> name{};
+    std::array<DataFieldSelection, MAX_DATA_FIELDS_PER_PAGE> fields{};
 };
 
 struct SmartShuntConfig {
@@ -25,6 +93,8 @@ struct AppSettings {
     DisplayTheme theme = DisplayTheme::Day;
     uint8_t day_brightness = 80;
     uint8_t night_brightness = 20;
+    UnitsSettings units{};
+    std::array<DataPageConfig, MAX_DATA_PAGES> pages{};
     std::array<SmartShuntConfig, MAX_SMARTSHUNTS> smartshunts{};
 };
 
