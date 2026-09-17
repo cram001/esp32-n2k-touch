45443f58b362a92a2ca60a43a3d7eaf8a12cbc94
cram001 <marcarchambault@gmail.com>
2026-09-17 09:40:12 -0700

    Add SmartShunt and NMEA bridge settings

--- src/app_settings.hpp
+++ src/app_settings.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 
 enum class DisplayTheme : uint8_t {
@@ -11,6 +12,12 @@ struct AppSettings {
     DisplayTheme theme = DisplayTheme::Day;
     uint8_t day_brightness = 80;
     uint8_t night_brightness = 20;
+
+    bool smartshunt_enabled = false;
+    bool smartshunt_n2k_enabled = false;
+    uint8_t battery_instance = 0;
+    std::array<char, 18> smartshunt_mac{};       // "AA:BB:CC:DD:EE:FF"
+    std::array<char, 33> smartshunt_bindkey{};   // 128-bit key as 32 hex chars
 };
 
 bool settings_init();
