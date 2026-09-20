556567be01483a469d2e5436608649ef561bb6f9
cram001 <marcarchambault@gmail.com>
2026-09-20 01:31:55 -0700

    Move NMEA transport to new ESP-IDF TWAI API

--- src/third_party/nmea2000_esp32xx/NMEA2000_esp32xx.h
+++ src/third_party/nmea2000_esp32xx/NMEA2000_esp32xx.h
@@ -1,47 +1,51 @@
 /*
-NMEA2000_esp32xx.h
+NMEA2000 ESP32-S3 TWAI transport.
 
-Copyright (c) 2015-2020 Timo Lappalainen, Kave Oy, www.kave.fi
+Derived from jiauka/NMEA2000_esp32xx.
+Copyright (c) 2015-2020 Timo Lappalainen, Kave Oy
 Copyright (c) 2023 Jaume Clarens "jiauka"
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
-Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+MIT License: permission is granted, free of charge, to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of this software,
+subject to retaining this copyright and permission notice.
 */
 
 #pragma once
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
 #include "driver/gpio.h"
+#include "esp_twai.h"
 #include "NMEA2000.h"
 #include "N2kMsg.h"
 
 class tNMEA2000_esp32xx : public tNMEA2000 {
 private:
+    struct tCANFrame {
+        uint32_t id = 0;
+        uint8_t len = 0;
+        uint8_t buf[8]{};
+    };
+
     bool is_open_ = false;
     static bool can_in_use_;
-
-protected:
     gpio_num_t tx_pin_;
     gpio_num_t rx_pin_;
+    twai_node_handle_t node_ = nullptr;
+    QueueHandle_t rx_queue_ = nullptr;
 
-    bool CANSendFrame(unsigned long id, unsigned char len, const unsigned char *buf, bool wait_sent=true) override;
+    static bool rx_done_callback(twai_node_handle_t handle,
+                                 const twai_rx_done_event_data_t *edata,
+                                 void *user_ctx);
+
+protected:
+    bool CANSendFrame(unsigned long id, unsigned char len,
+                      const unsigned char *buf, bool wait_sent=true) override;
     bool CANOpen() override;
-    bool CANGetFrame(unsigned long &id, unsigned char &len, unsigned char *buf) override;
+    bool CANGetFrame(unsigned long &id, unsigned char &len,
+                     unsigned char *buf) override;
 
 public:
-    explicit tNMEA2000_esp32xx(gpio_num_t tx_pin=GPIO_NUM_5, gpio_num_t rx_pin=GPIO_NUM_4);
+    explicit tNMEA2000_esp32xx(gpio_num_t tx_pin=GPIO_NUM_5,
+                               gpio_num_t rx_pin=GPIO_NUM_4);
 };
